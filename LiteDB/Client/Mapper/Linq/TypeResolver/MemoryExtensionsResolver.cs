86eefe8fda9c334a8877dc3571f57ff2f82bb03e
EB <n.baumgaertner@gmail.com>
2025-10-07 02:18:29 +0200

    Fix/2670 memoryextensions net10 followup (#2710)
    * Add .NET 10 target framework to test project
    
    * Added support for Contains 3-parameter overload
    
    * Fix CI
    
    ---------
    
    Co-authored-by: NB <n.baumgaertner@hotmail.com>
    Co-authored-by: JKamsker <11245306+JKamsker@users.noreply.github.com>

--- LiteDB/Client/Mapper/Linq/TypeResolver/MemoryExtensionsResolver.cs
+++ LiteDB/Client/Mapper/Linq/TypeResolver/MemoryExtensionsResolver.cs
@@ -6,11 +6,21 @@ namespace LiteDB
     {
         public string ResolveMethod(MethodInfo method)
         {
-            if (method.Name == nameof(System.MemoryExtensions.Contains))
-            {
-                var parameters = method.GetParameters();
+            if (method.Name != nameof(System.MemoryExtensions.Contains))
+                return null;
+            var parameters = method.GetParameters();
 
-                if (parameters.Length == 2)
+            if (parameters.Length == 2)
+            {
+                return "@0 ANY = @1";
+            }
+
+            // Support the 3-parameter overload only when comparer defaults to null.
+            if (parameters.Length == 3)
+            {
+                var third = parameters[2];
+
+                if (third.HasDefaultValue && third.DefaultValue == null)
                 {
                     return "@0 ANY = @1";
                 }
